adb49eb6df57bb9017279969e1994d2c71be4649
humanova <craxes_humanova@hotmail.com>
2019-02-15 21:44:13 +0300

    seperated /login and /app_login request

--- app.py
+++ app.py
@@ -15,8 +15,23 @@ except Exception as e:
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
+
 @app.route('/api/v1/login', methods=['POST'])
 def login():
+    username = request.json['username']
+    password = request.json['password']
+    pass_hash = str2md5(password)
+    
+    user = db.GetUser(username, pass_hash)
+
+    if not user == None:
+        user.last_login = datetime.datetime.now()
+        return jsonify(dict(success=True,userdata=dict(username=user.username,email=user.email,account_type = user.account_type, unique_id = user.unique_id, register_date=user.register_date)))
+    else:
+        return jsonify(dict(success=False))
+
+@app.route('/api/v1/app_login', methods=['POST'])
+def app_login():
     username = request.json['username']
     password = request.json['password']
     hwid = request.json['hwid']
